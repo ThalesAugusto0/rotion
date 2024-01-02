75285c79ed27ad704e0b91ea1cafef1a6088bf7f
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2024-01-02 09:09:32 -0300

    Feature: Configurando React Query, e exemplos de comunicação usando IPC

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,8 +1,19 @@
-import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge, ipcRenderer } from 'electron'
+import { ElectronAPI, electronAPI } from '@electron-toolkit/preload'
+
+declare global {
+  export interface Window {
+    electron: ElectronAPI
+    api: typeof api
+  }
+}
 
 // Custom APIs for renderer
-const api = {}
+const api = {
+  fetchDocuments(): Promise<Array<{ id: string; title: string }>> {
+    return ipcRenderer.invoke('fetch-documents')
+  },
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
@@ -15,8 +26,6 @@ if (process.contextIsolated) {
     console.error(error)
   }
 } else {
-  // @ts-ignore (define in dts)
   window.electron = electronAPI
-  // @ts-ignore (define in dts)
   window.api = api
 }
